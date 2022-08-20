0b5cc78cb01908c827ec8c09f4bb6e7cf882e443
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-08-20 21:18:01 +0530

    "Backend"

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = () => {
   return mongoose.connect(
     "mongodb+srv://rohitchourey0809:rohit$12345@cluster0.2j4drqb.mongodb.net/SyoftRegistration?retryWrites=true&w=majority"
-    // { useNewUrlParser: true, useUnifiedTopology: true }
+    
   );
 };
 
