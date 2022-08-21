9e9b296e7ef25cf1ff0d0b41f71486c977625fd5
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-08-21 15:15:49 +0530

    "Update""

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 var bodyParser = require("body-parser");
-const path = require("path");
+// const path = require("path");
 // const favicon = require("serve-favicon");
 const connect = require("./Config/db");
 const Usercontroller = require("./Controllers/usercontroller");
@@ -19,16 +19,16 @@ let port = process.env.PORT || 8080;
 app.use(express.json());
 
 // app.use(favicon(path.join(dirname, "build", "favicon.ico")));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(bodyParser.json());
 
 app.use("/users", Usercontroller);
 app.post("/register", register);
 app.post("/login", login);
 app.use("/product", Productcontroller);
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname + "/index.html"));
+// });
 app.post("/product", Productcontroller);
 
 app.listen(port, async () => {
